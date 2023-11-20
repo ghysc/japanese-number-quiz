e18213533d9d6cc7213625703c3cdaa75a534222
Cyril Ghys <cyril.ghys@gmail.com>
2023-11-20 12:39:03 +0100

    random number generator OK

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 
-function newNumber() {
+const min = 0;
+const max = 100000;
 
+document.addEventListener('DOMContentLoaded', function() {
+    displayNumber();
+    clearInput();
+});
+
+function clearInput() {
+    let answer = document.getElementById('answer');
+    if (answer) {
+        answer.value = '';
+    }
+}
+
+function displayNumber() {
+    let number = document.getElementById('number');
+    
+    if (number) {
+        let random = Math.floor(Math.random() * (max - min + 1)) + min;
+        number.innerText = random;
+    }
+}
+
+function checkAnswer(element) {
+    let answer = element.value;
 }
