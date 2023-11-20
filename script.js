12a770a6959643378742fb2f3d64e3e12957373d
Cyril Ghys <cyril.ghys@gmail.com>
2023-11-20 13:18:37 +0100

    live server, csv fetched

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 
 const min = 0;
 const max = 100000;
+let numbers = [];
 
 document.addEventListener('DOMContentLoaded', function() {
-    displayNumber();
     clearInput();
+    buildNumberQuizData();
+    displayNumber();
 });
 
 function clearInput() {
@@ -14,12 +16,30 @@ function clearInput() {
     }
 }
 
+function buildNumberQuizData() {
+    // Use the fetch API to get the content of the file
+    fetch('./numbers.csv')
+        .then(response => response.text())
+        .then(csvData => {
+            // Parse the CSV data using Papa Parse
+            Papa.parse(csvData, {
+                complete: function(results) {
+                    numbers = results.data;
+                    console.log(numbers);
+                }
+            });
+        })
+        .catch(error => console.error('Error fetching file:', error));
+}
+
 function displayNumber() {
     let number = document.getElementById('number');
     
     if (number) {
         let random = Math.floor(Math.random() * (max - min + 1)) + min;
         number.innerText = random;
+
+        
     }
 }
 
